308b3e06caccf29949f5e254c28f8b59e476a546
ppatil002 <pratikvpatil2002@gmail.com>
2023-12-09 19:14:33 +0530

    feat: Resource,Subject Pin Backend

---
+++ controllers/CurriculumDeveloper.js
@@ -0,0 +1,108 @@
+const { response } = require("express");
+const {
+  curriculumDeveloperAuth,
+  curriculumDeveloperFeatures,
+} = require("../classes/curriculumDeveloper");
+
+exports.GetAllSubjects = async (req, res, next) => {
+  try {
+    let ans = curriculumDeveloperFeatures.getAllSubjects();
+    try {
+      let subjects = [];
+      for (let i = 0; i < ans.length; i++) {
+        let n = {
+          subject_id: ans[i].subject_id,
+          name: ans[i].name,
+          department: ans[i].department,
+          list_resource_id: ans[i].list_resource_id,
+        };
+        subjects.push(n);
+      }
+      res.send({ subjects });
+    } catch (error) {
+      console.log(error);
+    }
+  } catch (error) {
+    console.log(error);
+    res.send({ error: "Subjects Not Found" });
+  }
+};
+
+exports.GetResourceBySubject = async (req, res, next) => {
+  try {
+    let ans = curriculumDeveloperFeatures.getResourceBySubject(subject_id);
+    try {
+      if (ans.length > 0) {
+        let resources = [];
+        for (let i = 0; i < ans.length; i++) {
+          let n = {
+            id: ans[i].id,
+            subject_id: ans[i].subject_id,
+            code: ans[i].code,
+            name: ans[i].name,
+            author: ans[i].author,
+          };
+          resources.push(n);
+        }
+      }
+      res.send({ resources });
+    } catch (error) {
+      console.log(error);
+    }
+  } catch (error) {
+    console.log(error);
+    res.send({ error: "Subjects Not Found" });
+  }
+};
+
+exports.AddPinnedResources = async (req, res, next) => {
+  try {
+    let { id, resource } = req.body;
+    let all_pinned_resources =
+      await curriculumDeveloperFeatures.getPinnedResourcesById(id);
+    if (all_pinned_resources[0].pinned_resources != null) {
+      let array = all_pinned_resources[0].pinned_resources.split("#");
+
+      if (array.indexOf(resource) == -1) {
+        let pinned_resources =
+          all_pinned_resources[0].pinned_resources + "#" + resource;
+        await curriculumDeveloperFeatures.addPinnedResources(
+          pinned_resources,
+          id
+        );
+      }
+    } else {
+      await curriculumDeveloperFeatures.addPinnedResources(resource, id);
+    }
+    res.send({ message: "Resource Added Sucessufully" });
+  } catch (error) {
+    console.log(error);
+    res.send({ error: "Cannot Add Resource" });
+  }
+};
+
+exports.AddPinnedSubjects = async (req, res, next) => {
+  try {
+    let { id, subject } = req.body;
+    let all_pinned_subject =
+      await curriculumDeveloperFeatures.getPinnedSubjectsById(id);
+    if (all_pinned_subject[0].pinned_subjects != null) {
+      let array = all_pinned_subject[0].pinned_subjects.split("#");
+
+      if (array.indexOf(subject) == -1) {
+        let pinned_subjects =
+          all_pinned_subject[0].pinned_subjects + "#" + subject;
+        await curriculumDeveloperFeatures.addPinnedSubjects(
+          pinned_subjects,
+          id
+        );
+      }
+    } else {
+      await curriculumDeveloperFeatures.addPinnedSubjects(subject, id);
+    }
+    res.send({ message: "Subject Added Sucessufully" });
+  } catch (error) {
+    console.log(error);
+    res.send({ error: "Cannot Add Subject" });
+  }
+};
--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const AICTEAdminRouter = require("./routes/AICTEAdmin");
+const CurriculumDeveloperRouter = require("./routes/CurriculumDeveloper");
 
-const {port} = require("./config/configKeys")
+const { port } = require("./config/configKeys");
 
 const app = express();
 
@@ -13,15 +14,15 @@ app.use(express.json());
 
 // Middleware
 app.use(express.json({ limit: "50mb" }));
-app.use("/api/AICTEAdmin", AICTEAdminRouter)
+app.use("/api/AICTEAdmin", AICTEAdminRouter);
+app.use("/api/CurriculumDeveloper", CurriculumDeveloperRouter);
 
 app.get("/api", (req, res) => {
   res.send("This is Shiksha Niyojak");
 });
 
-
-const PORT = port 
+const PORT = port;
 
 app.listen(PORT, () => {
-  console.log("✅ Server running on port:",PORT);
+  console.log("✅ Server running on port:", PORT);
 });
