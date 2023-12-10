802764fad900d3b0dd248c60ea03a77cca8f027e
abhi-1003 <abhikurule24@gmail.com>
2023-12-11 01:16:35 +0530

    feat: register-educator backend added

---
+++ routes/Educator.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const EducatorsControllers = require("../controllers/Educator");
+const router = express.Router();
+
+
+// router.post("/educatorLogin", EducatorsControllers.EducatorLogin);
+router.post("/educatorRegister", EducatorsControllers.EducatorRegistration);
+
+
+module.exports = router;
