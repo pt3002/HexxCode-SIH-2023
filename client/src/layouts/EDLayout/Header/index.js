9e8eabbedefb8b4be940207ac8bd52f079eefb60
abhi-1003 <abhikurule24@gmail.com>
2023-12-14 18:52:29 +0530

    signout and header changes in all roles

--- client/src/layouts/EDLayout/Header/index.js
+++ client/src/layouts/EDLayout/Header/index.js
@@ -18,7 +18,7 @@ import CloseTwoToneIcon from '@mui/icons-material/CloseTwoTone';
 import HeaderButtons from './Buttons';
 import HeaderUserbox from './Userbox';
 import HeaderMenu from './Menu';
-
+// ${alpha(theme.header.background, 0.95)}
 const HeaderWrapper = styled(Box)(
   ({ theme }) => `
         height: ${theme.header.height};
@@ -26,7 +26,7 @@ const HeaderWrapper = styled(Box)(
         padding: ${theme.spacing(0, 2)};
         right: 0;
         z-index: 6;
-        background-color: ${alpha(theme.header.background, 0.95)};
+        background-color: rgb(255, 200, 0);        
         backdrop-filter: blur(3px);
         position: fixed;
         justify-content: space-between;
