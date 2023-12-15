92edebfd703bb876bb00ea3af675dd92af63e161
pt_3002 <prerna_tulsiani@hotmail.com>
2023-12-15 23:17:40 +0530

    chore: df post page integration complete

--- client/src/content/pages/CD/postPage.js
+++ client/src/content/pages/CD/postPage.js
@@ -1,5 +1,5 @@
 import UserTokenContext from "../../../contexts/UserTokenContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState} from "react";
 import {
   Box,
   Button,
@@ -11,19 +11,24 @@ import {
   styled,
   Stack,
   CardActions,
+  Paper,
 } from "@mui/material";
 
 import { ArrowForwardTwoTone } from "@mui/icons-material";
-import CommentTwoToneIcon from '@mui/icons-material/CommentTwoTone';
-import ShareTwoToneIcon from '@mui/icons-material/ShareTwoTone';
-import ThumbUpAltTwoToneIcon from '@mui/icons-material/ThumbUpAltTwoTone';
+import CommentTwoToneIcon from "@mui/icons-material/CommentTwoTone";
+import ShareTwoToneIcon from "@mui/icons-material/ShareTwoTone";
+import ThumbUpAltTwoToneIcon from "@mui/icons-material/ThumbUpAltTwoTone";
 import Text from "../../../components/Text";
-import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import { useTheme } from "@mui/material";
+import axios from "axios"
+import { backendURL } from "../../../configKeys";
+import {fToNow} from "../../../utils/formatTime"
 
-import "./style.css"
+import "./style.css";
 
 const ButtonSuccess = styled(Button)(
-    ({ theme }) => `
+  ({ theme }) => `
        background: ${theme.colors.success.main};
        color: ${theme.palette.success.contrastText};
   
@@ -31,14 +36,22 @@ const ButtonSuccess = styled(Button)(
           background: ${theme.colors.success.dark};
        }
       `
-  );
+);
 
-  const CardActionsWrapper = styled(CardActions)(
-    ({ theme }) => `
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.colors.success.main,
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.success.contrastText,
+}));
+
+const CardActionsWrapper = styled(CardActions)(
+  ({ theme }) => `
        background: ${theme.colors.alpha.black[5]};
        padding: ${theme.spacing(3)};
   `
-  );
+);
 
 function PostPage() {
   const addresses = {
@@ -46,73 +59,120 @@ function PostPage() {
     shipping: 8,
   };
 
+  const theme = useTheme();
+
   const userContext = useContext(UserTokenContext);
   const { dict, checkToken } = userContext;
 
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    checkToken()
+    axios.get(backendURL + "/Forum/getAllPost").then((res) => {
+        setPosts(res.data.all_posts)
+    })
+  }, [])
+
+  console.log(posts)
+
   return (
     <>
-      <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="stretch"
-        spacing={3}
-      >
-        <Grid item xs={12} sm={12}>
-          <Card>
-            <CardHeader
-              title="Delivery Addresses"
-            />
-            <Divider />
-            <Box p={2}>
-              <Typography variant="subtitle1" fontWeight="bold">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </Typography>
-              <Box sx={{ minHeight: { xs: 0 } }} py = {2}>
-                <Typography variant="h5" fontWeight="bold" fontSize="16px">Related Tags : </Typography>
-                
-              </Box>
-            </Box>
-            <Divider />
-      <CardActionsWrapper
-        sx={{
-          display: { xs: 'block', md: 'flex' },
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          py : 1,
-          paddingLeft:0.5,
-          paddingRight : 1.5
-        }}
-      >
-        <Box>
-            <Button diabled startIcon = {<AccessTimeIcon />} sx = {{marginRight:2}}>5 Days Ago</Button>
-          <Button startIcon={<ThumbUpAltTwoToneIcon />} variant="contained">
-            Like
-          </Button>
-          <Button
-            startIcon={<CommentTwoToneIcon />}
-            variant="outlined"
-            sx={{ mx: 2 }}
+    {
+        posts.length > 0  && posts.map((post, index) => {
+           return(
+            <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="stretch"
+            spacing={3}
+            sx = {{my:1.5}}
           >
-            Reply
-          </Button>
-        </Box>
-        <Box sx={{ mt: { xs: 2, md: 0 } }}>
-          <Typography variant="subtitle2" component="span">
-            <Text color="black">
-              <b>485</b>
-            </Text>{' '}
-            reactions •{' '}
-            <Text color="black">
-              <b>63</b>
-            </Text>{' '}
-            comments
-          </Typography>
-        </Box>
-      </CardActionsWrapper>
-          </Card>
-        </Grid>
-      </Grid>
+            <Grid item xs={12} sm={12}>
+              <Card>
+                <CardHeader title={post.title} />
+                <Divider />
+                <Box p={2}>
+                  <Typography variant="subtitle1" fontWeight="bold">
+                    {post.description}
+                  </Typography>
+                  <Box sx={{ minHeight: { xs: 0 } }} py={2}>
+                    <Stack direction="row" spacing={2}>
+                      <Typography
+                        variant="h5"
+                        fontWeight="bold"
+                        fontSize="16px"
+                        sx={{ py: `${theme.spacing(1)}` }}
+                      >
+                        
+                        Related Tags :{" "}
+                      </Typography>
+                      {
+                            post.tags.map((tag) => {
+                                return(
+                                    <Item>{tag.name}</Item>
+                                )
+                                
+                            })
+                        }
+                    </Stack>
+                  </Box>
+                </Box>
+                <Divider />
+                <CardActionsWrapper
+                  sx={{
+                    display: { xs: "block", md: "flex" },
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    py: 1,
+                    paddingLeft: 0.5,
+                    paddingRight: 1.5,
+                  }}
+                >
+                  <Box>
+                    <Button
+                      diabled
+                      startIcon={<AccessTimeIcon />}
+                      sx={{ marginRight: 2 }}
+                    >
+                      {fToNow(post.time)}
+                    </Button>
+                    <Button
+                      startIcon={<ThumbUpAltTwoToneIcon />}
+                      variant="contained"
+                    >
+                      Like
+                    </Button>
+                    <Button
+                      startIcon={<CommentTwoToneIcon />}
+                      variant="outlined"
+                      sx={{ mx: 2 }}
+                    >
+                      Reply
+                    </Button>
+                  </Box>
+                  <Box sx={{ mt: { xs: 2, md: 0 } }}>
+                    <Typography variant="subtitle2" component="span">
+                      <Text color="black">
+                        <b>{post.views}</b>
+                      </Text>{" "}
+                      views •{" "}
+                      <Text color="black">
+                        <b>63</b>
+                      </Text>{" "}
+                      comments
+                    </Typography>
+                  </Box>
+                </CardActionsWrapper>
+              </Card>
+            </Grid>
+          </Grid>
+           )
+        })
+    }
+
+
+
     </>
   );
 }
